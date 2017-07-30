ee051b5035311d32d6088546a6c48a4d568976f0
hallos <oscar.hall@outlook.com>
2017-07-30 15:26:23 +0200

    Rebuilt script to trigger CLI commands when turning on switch in HomeKit

--- index.js
+++ index.js
@@ -1,44 +1,39 @@
 "use strict";
 
 var Service, Characteristic;
+var exec = require('child_process').exec;
 
 module.exports = function(homebridge) {
 
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
 
-  homebridge.registerAccessory("homebridge-dummy", "DummySwitch", DummySwitch);
+  homebridge.registerAccessory("homebridge-cmdtrigger", "CmdTrigger", CmdTrigger);
 }
 
-function DummySwitch(log, config) {
+function CmdTrigger(log, config) {
   this.log = log;
   this.name = config.name;
-  this.stateful = config.stateful;
-  this.reverse = config.reverse;
-  
+  this.command = config.command;
+
   this._service = new Service.Switch(this.name);
   this._service.getCharacteristic(Characteristic.On)
     .on('set', this._setOn.bind(this));
-
-  if (this.reverse) this._service.setCharacteristic(Characteristic.On, true);
 }
 
-DummySwitch.prototype.getServices = function() {
+CmdTrigger.prototype.getServices = function() {
   return [this._service];
 }
 
-DummySwitch.prototype._setOn = function(on, callback) {
-
-  this.log("Setting switch to " + on);
-
-  if (on && !this.reverse && !this.stateful) {
+CmdTrigger.prototype._setOn = function(on, callback) {
+  if (on) {
+    //Execute command from config file
+    exec(this.command);
+    this.log("Command executed: '" + this.command + "'");
+    //Turn off switch again after 500ms
     setTimeout(function() {
       this._service.setCharacteristic(Characteristic.On, false);
-    }.bind(this), 1000);
-  } else if (!on && this.reverse && !this.stateful) {
-    setTimeout(function() {
-      this._service.setCharacteristic(Characteristic.On, true);
-    }.bind(this), 1000);
+    }.bind(this), 500);
   }
   callback();
 }
